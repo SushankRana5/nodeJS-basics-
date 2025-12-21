dc1de84db349275ccef62c151e48d44d6013cc53
unknown <ranasushank5@gmail.com>
2025-12-21 13:08:36 +0545

    _

--- nodejs/cookies and session/index.js
+++ nodejs/cookies and session/index.js
@@ -3,24 +3,20 @@ const app = express();
 const saltRounds = 10;
 import bcrypt from 'bcrypt'
 
-app.get('/', (req, res) => {
-    bcrypt.genSalt(saltRounds, function (err, salt) {
-        bcrypt.hash("hahahahahaah", salt, (err, hash) => {
-            console.log(salt);
-        });
-    });
-
-
-    bcrypt.compare("hahahahahaah", "$2b$10$XvKy8vY5m9iXnE8rrvy/Re", (err, result) => {
-        console.log(result);
-    });
-
-    res.send('creating salt and hash')
-})
-// app.get('/compare', (req, res) => {
-//     bcrypt.compare("hahahahahaah", "$2b$10$XvKy8vY5m9iXnE8rrvy/Re", (err, result)=> {
-//         console.log(result);
+// app.get('/', (req, res) => {
+//     bcrypt.genSalt(saltRounds, function (err, salt) {
+//         bcrypt.hash("hahahahahaah", salt, (err, hash) => {
+//             console.log(salt);
+//         });
 //     });
-//     res.send('comparing password')
+//     res.send('creating salt and hash')
 // })
+app.get('/', (req, res) => {
+    bcrypt.compare("hahahahahaah", "$2b$10$MjCaUOUM7bhNWi//YEV5eO", (err, result)=> {
+        console.log(result);
+        console.log(err);
+        
+    });
+    res.send('comparing password')
+})
 app.listen(3000);
